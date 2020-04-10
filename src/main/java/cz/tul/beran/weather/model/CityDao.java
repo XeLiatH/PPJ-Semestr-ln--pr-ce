da18b33c3f13d4534f4ec390e6bfae5a967e25f4
Matěj Beran <matej.beran96@gmail.com>
2020-04-10 10:18:06 +0200

    Improve dao objects.

--- src/main/java/cz/tul/beran/weather/model/CityDao.java
+++ src/main/java/cz/tul/beran/weather/model/CityDao.java
@@ -1,5 +1,6 @@
 package cz.tul.beran.weather.model;
 
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 
 import java.sql.ResultSet;
@@ -7,28 +8,45 @@ import java.util.List;
 
 public class CityDao {
 
-    private final NamedParameterJdbcOperations jdbc;
+  private final NamedParameterJdbcOperations jdbc;
 
-    public CityDao(NamedParameterJdbcOperations jdbc) {
-        this.jdbc = jdbc;
-    }
+  public CityDao(NamedParameterJdbcOperations jdbc) {
+    this.jdbc = jdbc;
+  }
 
-    public List<City> getCities() {
-        return jdbc.query(
-                "select country.id as cid, country.name as cname, country.code as ccode, city.id as id, city.name as name from city, country",
-                (ResultSet rs, int row) -> {
-                    Country country = new Country();
-                    country.setId(rs.getInt("cid"));
-                    country.setName(rs.getString("cname"));
-                    country.setCode(rs.getString("ccode"));
+  public List<City> getCities() {
+    return jdbc.query(
+        "select country.id as cid, country.name as cname, country.code as ccode, city.id as id, city.name as name from city, country",
+        (ResultSet rs, int row) -> {
+          Country country = new Country();
+          country.setId(rs.getInt("cid"));
+          country.setName(rs.getString("cname"));
+          country.setCode(rs.getString("ccode"));
 
-                    City city = new City();
-                    city.setId(rs.getInt("id"));
-                    city.setName(rs.getString("name"));
-                    city.setCountry(country);
+          City city = new City();
+          city.setId(rs.getInt("id"));
+          city.setName(rs.getString("name"));
+          city.setCountry(country);
 
-                    return city;
-                }
-            );
-    }
+          return city;
+        });
+  }
+
+  public boolean createCity(City city) {
+    BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(city);
+
+    return jdbc.update("insert into city (name) values (:name)", params) == 1;
+  }
+
+  public boolean updateCity(City city) {
+      BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(city);
+
+      return jdbc.update("update city set name=:name where id=:id", params) == 1;
+  }
+
+  public boolean deleteCity(City city) {
+      BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(city);
+
+      return jdbc.update("delete from offer where id=:id", params) == 1;
+  }
 }
--- src/main/java/cz/tul/beran/weather/model/CountryDao.java
+++ src/main/java/cz/tul/beran/weather/model/CountryDao.java
@@ -1,5 +1,6 @@
 package cz.tul.beran.weather.model;
 
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 
 import java.sql.ResultSet;
@@ -7,23 +8,40 @@ import java.util.List;
 
 public class CountryDao {
 
-    private final NamedParameterJdbcOperations jdbc;
+  private final NamedParameterJdbcOperations jdbc;
 
-    public CountryDao(NamedParameterJdbcOperations jdbc) {
-        this.jdbc = jdbc;
-    }
+  public CountryDao(NamedParameterJdbcOperations jdbc) {
+    this.jdbc = jdbc;
+  }
 
-    public List<Country> getCountries() {
-        return jdbc.query(
-                "select * from country",
-                (ResultSet rs, int row) -> {
-                    Country country = new Country();
-                    country.setId(rs.getInt("id"));
-                    country.setName(rs.getString("name"));
-                    country.setCode(rs.getString("code"));
+  public List<Country> getCountries() {
+    return jdbc.query(
+        "select * from country",
+        (ResultSet rs, int row) -> {
+          Country country = new Country();
+          country.setId(rs.getInt("id"));
+          country.setName(rs.getString("name"));
+          country.setCode(rs.getString("code"));
 
-                    return country;
-                }
-            );
-    }
+          return country;
+        });
+  }
+
+  public boolean createCountry(Country country) {
+    BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(country);
+
+    return jdbc.update("insert into country (name, code) values (:name, :code)", params) == 1;
+  }
+
+  public boolean updateCountry(Country country) {
+    BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(country);
+
+    return jdbc.update("update country set name=:name, code=:code where id=:id", params) == 1;
+  }
+
+  public boolean deleteCountry(Country country) {
+      BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(country);
+
+      return jdbc.update("delete from country where id=:id", params) == 1;
+  }
 }

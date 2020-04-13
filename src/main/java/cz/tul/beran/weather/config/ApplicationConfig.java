2ff4fb102a965e5687afb2af2a379601d8e979a9
Matěj Beran <matej.beran96@gmail.com>
2020-04-13 14:41:47 +0200

    Last improvements with hsql implementation.

--- src/main/java/cz/tul/beran/weather/config/ApplicationConfig.java
+++ src/main/java/cz/tul/beran/weather/config/ApplicationConfig.java
@@ -2,11 +2,19 @@ package cz.tul.beran.weather.config;
 
 import cz.tul.beran.weather.model.CityDao;
 import cz.tul.beran.weather.model.CountryDao;
+import cz.tul.beran.weather.provisioning.Provisioner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ApplicationConfig {
+
+  @Profile({"dev", "test"})
+  @Bean(initMethod = "doProvision")
+  public Provisioner provisioner() {
+    return new Provisioner();
+  }
 
   @Bean
   public CityDao cityDao() {

3ea0b22fc51a89c3f3ccef08850c8450ab254b39
Matěj Beran <matej.beran96@gmail.com>
2020-05-11 09:58:15 +0200

    More tests!

--- src/test/java/cz/tul/beran/weather/controller/rest/CountryControllerTest.java
+++ src/test/java/cz/tul/beran/weather/controller/rest/CountryControllerTest.java
@@ -5,8 +5,8 @@ import cz.tul.beran.weather.entity.mysql.Country;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.Order;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -18,12 +18,49 @@ public class CountryControllerTest extends AbstractTest {
   }
 
   @Test
+  @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:countries.sql")
+  @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:truncate.sql")
+  public void getCountries_ok() throws Exception {
+    String url = "/countries";
+
+    MvcResult mvcResult =
+        mvc.perform(MockMvcRequestBuilders.get(url).accept(MediaType.APPLICATION_JSON)).andReturn();
+
+    int status = mvcResult.getResponse().getStatus();
+    Assert.assertEquals(200, status);
+
+    String content = mvcResult.getResponse().getContentAsString();
+    Country[] countries = super.mapFromJson(content, Country[].class);
+
+    Assert.assertTrue(countries.length > 0);
+  }
+
+  @Test
+  @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:countries.sql")
+  @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:truncate.sql")
+  public void getCountry_ok() throws Exception {
+    String url = "/countries/1";
+
+    MvcResult mvcResult =
+        mvc.perform(MockMvcRequestBuilders.get(url).accept(MediaType.APPLICATION_JSON)).andReturn();
+
+    int status = mvcResult.getResponse().getStatus();
+    Assert.assertEquals(200, status);
+
+    String content = mvcResult.getResponse().getContentAsString();
+    Country country = super.mapFromJson(content, Country.class);
+
+    Assert.assertNotNull(country);
+  }
+
+  @Test
+  @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:truncate.sql")
   public void createCountry_ok() throws Exception {
     String url = "/countries";
 
     CountryDTO dto = new CountryDTO();
-    dto.setName("Test");
-    dto.setCode("te");
+    dto.setName("Czech Republic");
+    dto.setCode("CZ");
 
     String input = super.mapToJson(dto);
 
@@ -39,18 +76,85 @@ public class CountryControllerTest extends AbstractTest {
   }
 
   @Test
-  public void getCountries_ok() throws Exception {
+  @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:truncate.sql")
+  public void createCountry_bad() throws Exception {
     String url = "/countries";
 
+    CountryDTO dto = new CountryDTO();
+    dto.setName("Czech Republic");
+
+    String input = super.mapToJson(dto);
+
     MvcResult mvcResult =
-        mvc.perform(MockMvcRequestBuilders.get(url).accept(MediaType.APPLICATION_JSON)).andReturn();
+        mvc.perform(
+                MockMvcRequestBuilders.post(url)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(input))
+            .andReturn();
+
+    int status = mvcResult.getResponse().getStatus();
+    Assert.assertEquals(400, status);
+  }
+
+  @Test
+  @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:countries.sql")
+  @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:truncate.sql")
+  public void updateCountry_ok() throws Exception {
+    String url = "/countries/1";
+
+    CountryDTO dto = new CountryDTO();
+    dto.setName("Czech Rep");
+    dto.setCode("CZ");
+
+    String input = super.mapToJson(dto);
+
+    MvcResult mvcResult =
+        mvc.perform(
+                MockMvcRequestBuilders.put(url)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(input))
+            .andReturn();
 
     int status = mvcResult.getResponse().getStatus();
     Assert.assertEquals(200, status);
 
     String content = mvcResult.getResponse().getContentAsString();
-    Country[] cities = super.mapFromJson(content, Country[].class);
+    Country country = super.mapFromJson(content, Country.class);
 
-    Assert.assertTrue(cities.length > 0);
+    Assert.assertNotNull(country);
+  }
+
+  @Test
+  @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:countries.sql")
+  @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:truncate.sql")
+  public void updateCountry_bad() throws Exception {
+    String url = "/countries/1";
+
+    CountryDTO dto = new CountryDTO();
+    dto.setName("Czech Republic");
+
+    String input = super.mapToJson(dto);
+
+    MvcResult mvcResult =
+        mvc.perform(
+                MockMvcRequestBuilders.put(url)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(input))
+            .andReturn();
+
+    int status = mvcResult.getResponse().getStatus();
+    Assert.assertEquals(400, status);
+  }
+
+  @Test
+  @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:countries.sql")
+  @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:truncate.sql")
+  public void deleteCountry_ok() throws Exception {
+    String url = "/countries/1";
+
+    MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(url)).andReturn();
+
+    int status = mvcResult.getResponse().getStatus();
+    Assert.assertEquals(200, status);
   }
 }
